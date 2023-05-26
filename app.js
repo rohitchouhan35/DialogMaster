4f56cf4e773a5aa3885b74bbd996e18483be8f47
rohit <rohitchouhan3512@gmail.com>
2023-05-26 23:33:44 +0530

    basic-chatGPT response

---
+++ app.js
@@ -0,0 +1,61 @@
+
+const submitButton = document.querySelector('#submit');
+const outPutElement = document.querySelector('#output');
+const inputElement = document.querySelector('input');
+const historyElement = document.querySelector('.history');
+const buttonElement = document.querySelector('button');
+
+const API_KEY = "your api key";
+
+const username = "rohitchouhan35";
+document.querySelector('nav p').textContent = username;
+
+function changeInput(value) {
+    const inputElement = document.querySelector('input');
+    inputElement.value = value;
+}
+
+async function getMessage() {
+    console.log('clicked');
+
+    try {
+        
+        const options = {
+            method: 'POST',
+            headers: {
+                'Authorization': `Bearer ${API_KEY}`,
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                model: "gpt-3.5-turbo",
+                messages: [{'role': "user", 'content': inputElement.value}],
+                max_tokens: 100
+            })
+              
+        }
+        const response = await fetch('https://api.openai.com/v1/chat/completions', options);
+
+        const data = await response.json(); 
+        console.log(data);
+        outPutElement.textContent = data.choices[0].message.content
+
+        if(data.choices[0].message.content && inputElement.value) {
+            const pElement = document.createElement('p');
+            pElement.textContent = inputElement.value;
+            pElement.addEventListener('click', () => changeInput(pElement.textContent))
+            historyElement.append(pElement);
+        }
+
+    } 
+    catch (error) {
+        console.error(error);
+    }
+}
+
+submitButton.addEventListener('click', getMessage);
+
+function clearInput() {
+    inputElement.value = ''
+}
+
+buttonElement.addEventListener('click', clearInput);
